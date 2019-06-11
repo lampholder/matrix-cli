39d10a8c2b0e884649516697a37f430e69e31fea
Thomas Lant <lampholder@gmail.com>
2019-06-11 10:10:43 +0000

    tidying the interface

--- matrix-cli/matrix_cli.py
+++ matrix-cli/matrix_cli.py
@@ -106,63 +106,68 @@ class MatrixAdmin:
                            password,
                            sync=False)
 
-    def deactivate_user(self, user_id):
+    def deactivate_user(self, user_id, reason):
         access_token = self._client.token
 
         deactivate = MatrixAdminAPI.deactivate_user(access_token, user_id)
-        logging.info('Deactivating account \'%s\': %s' % (
+        logging.info('Deactivating account \'%s\': %s\nReason: %s' % (
             user_id,
-            'SUCCESS' if deactivate.status_code == 200 else 'FAILURE'
+            'SUCCESS' if deactivate.status_code == 200 else 'FAILURE',
+            reason
         ))
         if deactivate.status_code != 200:
             logging.error(deactivate.text)
 
-    def park_alias(self, alias, parking_room_id='!GrRRLYVADFUBjqnEwX:matrix.org'):
+    def park_alias(self, alias, reason, parking_room_id='!GrRRLYVADFUBjqnEwX:matrix.org'):
         access_token = self._client.token
 
         room_id = MatrixAdminAPI.get_alias(access_token, alias).json().get('room_id')
         delete_alias = MatrixAdminAPI.delete_alias(access_token, alias)
-        logging.info('Removing alias \'%s\' from \'%s\': %s' % (
+        logging.info('Removing alias \'%s\' from \'%s\': %s\nReason: %s' % (
             alias,
             room_id,
-            'SUCCESS' if delete_alias.status_code == 200 else 'FAILURE'
+            'SUCCESS' if delete_alias.status_code == 200 else 'FAILURE',
+            reason
         ))
         if delete_alias.status_code != 200:
             logging.error(delete_alias.text)
 
         create_alias = MatrixAdminAPI.put_alias(access_token, alias, parking_room_id)
-        logging.info('Parking alias \'%s\' (parking spot: \'%s\'): %s' % (
+        logging.info('Parking alias \'%s\' (parking spot: \'%s\'): %s\nReason: %s' % (
             alias,
             parking_room_id,
-            'SUCCESS' if create_alias.status_code == 200 else 'FAILURE'
+            'SUCCESS' if create_alias.status_code == 200 else 'FAILURE',
+            reason
         ))
         if create_alias.status_code != 200:
             logging.error(create_alias.text)
 
-    def assign_alias(self, room_id, alias):
+    def assign_alias(self, room_id, alias, reason):
         access_token = self._client.token
 
         assign_alias = MatrixAdminAPI.put_alias(access_token,
                                                 alias,
                                                 room_id)
-        logging.info('Assigning alias \'%s\' to \'%s\': %s' % (
+        logging.info('Assigning alias \'%s\' to \'%s\': %s\nReason: %s' % (
             alias,
             room_id,
-            'SUCCESS' if assign_alias.status_code == 200 else 'FAILURE'
+            'SUCCESS' if assign_alias.status_code == 200 else 'FAILURE',
+            reason
         ))
         if assign_alias.status_code != 200:
             logging.error(assign_alias.text)
 
-    def delete_alias(self, alias):
+    def delete_alias(self, alias, reason):
         access_token = self._client.token
 
         room_id = self.resolve_alias(alias)
 
         delete_alias = MatrixAdminAPI.delete_alias(access_token, alias)
-        logging.info('Deleting alias \'%s\' from \'%s\': %s' % (
+        logging.info('Deleting alias \'%s\' from \'%s\': %s\nReason: %s' % (
             alias,
             room_id,
-            'SUCCESS' if delete_alias.status_code == 200 else 'FAILURE'
+            'SUCCESS' if delete_alias.status_code == 200 else 'FAILURE',
+            reason
         ))
         if delete_alias.status_code != 200:
             logging.error(delete_alias.text)
@@ -172,18 +177,19 @@ class MatrixAdmin:
 
         return MatrixAdminAPI.get_alias(access_token, alias).json().get('room_id')
 
-    def delist_room(self, room_id):
+    def delist_room(self, room_id, reason):
         access_token = self._client.token
 
         delist_room = MatrixAdminAPI.delete_room_listing(access_token, room_id)
-        logging.info('Delisting room \'%s\': %s' % (
+        logging.info('Delisting room \'%s\': %s\nReason: %s' % (
             room_id,
-            'SUCCESS' if delist_room.status_code == 200 else 'FAILURE'
+            'SUCCESS' if delist_room.status_code == 200 else 'FAILURE',
+            reason
         ))
         if delist_room.status_code != 200:
             logging.error(delist_room.text)
 
-    def shutdown_room_and_quarantine_media(self, room_id,
+    def shutdown_room_and_quarantine_media(self, room_id, reason,
                                            message=None,
                                            new_room_name='Room unavailable'):
         if message is None:
@@ -196,21 +202,23 @@ class MatrixAdmin:
         shutdown = MatrixAdminAPI.shutdown_room(access_token, user_id, room_id,
                                                 message, new_room_name)
         log_string = ('Shutting down room \'%s\' ' +
-                      '(new room name \'%s\'; message: \'%s\'): %s')
+                '(new room name \'%s\'; message: \'%s\'): %s\nReason: %s')
         logging.info(log_string % (
             room_id,
             new_room_name,
             message,
-            'SUCCESS' if shutdown.status_code == 200 else 'FAILURE'
+            'SUCCESS' if shutdown.status_code == 200 else 'FAILURE',
+            reason
         ))
         if shutdown.status_code != 200:
             logging.error(shutdown.text)
 
         quarantine = MatrixAdminAPI.quarantine_media(access_token, room_id)
-        logging.info('Quarantining media in \'%s\'; quarantining %s items: %s' % (
+        logging.info('Quarantining media in \'%s\'; quarantining %s items: %s\nReason: %s' % (
             room_id,
             quarantine.json().get('num_quarantined', 0),
-            'SUCCESS' if quarantine.status_code == 200 else 'FAILURE'
+            'SUCCESS' if quarantine.status_code == 200 else 'FAILURE',
+            reason
         ))
         if quarantine.status_code != 200:
             logging.error(quarantine.text)
